9bfbd58c68e06727c6e13c88e92b3d3a0b3810d6
Fahad <205fahadmohammad@gmail.com>
2023-06-07 18:31:09 +0600

    new setup done with TS

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+const port = process.env.PORT || 5000;
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/attendance-system");
+    console.log("DB connected");
+
+    app.listen(port, () => {
+      console.log(`Application listening on ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to connect", error);
+  }
+}
+
+main().catch((error) => console.log("from main function", error));
