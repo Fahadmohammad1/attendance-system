6f3163e1e6203196991d89badea9d5e60f022f37
Fahad <205fahadmohammad@gmail.com>
2023-07-11 21:55:41 +0600

    gracefully server close done

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { Server } from 'http'
+
+process.on('uncaughtException', error => {
+  console.error(error)
+  process.exit(1)
+})
+
+let server: Server
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
     console.log('DB connected')
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Application listening on ${config.port}`)
     })
   } catch (error) {
@@ -15,4 +23,22 @@ async function main() {
   }
 }
 
+process.on('unhandledRejection', error => {
+  if (server) {
+    server.close(() => {
+      console.error(error)
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+})
+
 main().catch(error => console.log('from main function', error))
+
+// process.on('SIGTERM', () => {
+//   logger.info('SIGTERM is received')
+//   if (server) {
+//     server.close()
+//   }
+// })
